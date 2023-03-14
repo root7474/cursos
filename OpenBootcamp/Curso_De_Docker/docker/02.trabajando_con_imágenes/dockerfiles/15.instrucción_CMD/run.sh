ecb58ad3fae3491a23204a667ebe8c9f8b6a5d39
Da7474 <davidrodriguezbolanos777@hotmail.com>
2023-03-14 16:28:00 -0500

    Eliminar curso de Docker

--- OpenBootcamp/Curso_De_Docker/docker/02.trabajando_con_imágenes/dockerfiles/15.instrucción_CMD/run.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-echo "Inciando container...."
-# una vez lanzado el contenedor podremos acceder a localhost:80/ para ver el resultado
-echo "INICIADO!!" > /var/www/html/ini.html
-
-apachectl -DFOREGROUND
--- OpenBootcamp/Curso_De_Docker/docker/06.docker_compose/proyectos/12.política_de_reinicio/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Estoy vivo"
-sleep 5
-echo "Estoy muerto"
