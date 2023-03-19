d8b80a20e3da17cebaa3ecf81062cc7fa9ba4959
Da7474 <davidrodriguezbolanos777@hotmail.com>
2023-03-19 13:01:26 -0500

    Módulo 1 Curso de JavaScript de OpenBootcamp

---
+++ OpenBootcamp/React_Frontend/Curso_De_JavaScript/1_Mi_Primer_Proyecto_De_Node/index.js
@@ -0,0 +1,5 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+// Este va a ser el mensaje que se envía para despedir
+console.log("Adiós");
---
+++ OpenBootcamp/React_Frontend/Practicas/Curso_De_JavaScript/Ejercicio_1/index.js
@@ -0,0 +1,3 @@
+// Mensaje de bienvenida a Node.js en OpenBootcamp
+// Autor: David Rodríguez
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
