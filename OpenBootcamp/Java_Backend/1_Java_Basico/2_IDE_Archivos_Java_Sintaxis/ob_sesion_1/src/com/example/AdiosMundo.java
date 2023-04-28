11eea03eb73c9549eacb55f0bb2d1cbb8a7da319
Da7474 <davidrodriguezbolanos777@hotmail.com>
2023-04-28 13:52:13 -0500

    Módulo 2 curso de Javav OB

---
+++ OpenBootcamp/Java_Backend/1_Java_Basico/2_IDE_Archivos_Java_Sintaxis/ob_sesion_1/src/com/example/AdiosMundo.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * Clase de prueba para crear un programa básico en Java
+ */
+
+public class AdiosMundo {
+    /**
+     * Punto de entrada a la aplicación Java
+     * Permite ejecutar la aplicación
+     * @param args - argumentos de entrada para la aplicación
+     */
+
+    public static void main(String[] args) {
+        // Esto es un comentario
+        System.out.println("Adios mundo cruel!!!"); // Esto es otro comentario
+
+        /* 
+         * Esto es un comentario multi línea
+         */
+    }
+}
---
+++ OpenBootcamp/Java_Backend/1_Java_Basico/2_IDE_Archivos_Java_Sintaxis/ob_sesion_1/src/com/example/Tipos.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Tipos {
+    public static void main(String[] args) {
+        // Tipos primitivos
+        // Los tipos primitivos no puedeneser nulos
+
+        // 1.) Numericos
+        // 1.1.) Enteros
+        byte variable1 = 4;
+        short variable2 = 10;
+        int variable3 = 30;
+        long numeroTelefono = 3167002210l;
+
+        // 1.2.) Decimales
+        float vaiable5 = 5.5f;
+        double precioSilla = 49.99;
+
+        // 2.) Booleanos
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // 3.) Texto
+        char variable9 = 'a';
+        String variable10 = "Hola cómo estás?";
+    }
+}
