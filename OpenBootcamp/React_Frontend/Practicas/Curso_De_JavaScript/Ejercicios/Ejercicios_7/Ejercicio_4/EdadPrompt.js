06afcd36efdd5bf98858d79262bcbbd444081ecc
Da7474 <davidrodriguezbolanos777@hotmail.com>
2023-05-01 17:44:28 -0500

    Agrego Ejercicio 4 Módulo 7 curso JavaScript OB

---
+++ OpenBootcamp/React_Frontend/Practicas/Curso_De_JavaScript/Ejercicios/Ejercicios_7/Ejercicio_4/EdadPrompt.js
@@ -0,0 +1,3 @@
+// Ejercicio 4: Preguntar edad a través del prompt
+// Autor: David Rodríguez
+let edad = prompt("Cuál es tuedad?:"); // Creamos una varible llamada "edad" y guardamos la información que ingrese el usuario en el prompt
