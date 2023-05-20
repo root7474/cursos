07596b015f9e4b5e35ba351fdea9292c78a758d7
Da7474 <davidrodriguezbolanos777@hotmail.com>
2023-05-20 03:37:29 -0500

    Elimino ficheros Curso_C#_NIvel_Basico

--- Udemy/Curso_De_C#_Udemy/C#_Nivel_Basico/OOP/Program_2.cs
+++
@@ -1,8 +0,0 @@
-namespace OOP
-{
-    class Player
-    {
-        public string name = "Player_01";
-        public int number = 1;
-    }
-}
--- Udemy/Curso_De_C#_Udemy/C#_Nivel_Basico/OOP_Inheritance/PL01.cs
+++
@@ -1,7 +0,0 @@
-namespace Inheritance
-{
-    class PL01 : Player
-    {
-        public int playerID = 1100;
-    }
-}
