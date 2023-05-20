5dfaa31c4305193867e8a75f3065c248b2a39623
Da7474 <davidrodriguezbolanos777@hotmail.com>
2023-05-20 03:51:18 -0500

    Actualizo archivo para convertir imágenes

--- convertir a ico/Main.py
+++ convertir a ico/Main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 def main():
-    im = Image.open(input("Digita la ruta de la imagen exportar: "))
+    im = Image.open(input("Imagen a exportar: "))
     im.save(input("Nombre de la imagen nueva: "))
 
 if __name__ == "__main__":
